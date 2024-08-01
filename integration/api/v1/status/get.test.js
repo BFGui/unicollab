bd812057621fcbb498dcd954d4f8dcfe28955150
BFGui <84551468+BFGui@users.noreply.github.com>
2024-08-01 03:03:29 +0000

    finaliza endpoint `/api/v1/status`

--- integration/api/v1/status/get.test.js
+++ integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3001/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  //teste da versão do DB
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  //teste de conexões
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  
+  //teste de conexões abertas
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
