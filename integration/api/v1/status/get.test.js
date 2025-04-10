847e1437d1fa8348356a5420537f40c8d65c5ca1
BFGui <84551468+BFGui@users.noreply.github.com>
2025-04-10 19:03:38 +0000

    add

--- integration/api/v1/status/get.test.js
+++ integration/api/v1/status/get.test.js
@@ -9,11 +9,11 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
   //teste da versão do DB
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("17.4");
 
   //teste de conexões
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.max_connections).toEqual(901);
   
   //teste de conexões abertas
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(0);
 });
