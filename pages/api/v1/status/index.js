48e5209208a4f1b7248a06ccb03f13607a9555bf
Guilherme Bretanha <84551468+BFGui@users.noreply.github.com>
2025-05-28 13:12:44 +0000

    run fix lint prettier

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,15 +4,14 @@ async function status(request, response) {
   //return response.status(500).json({ error: 'Internal Server Error' });
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query(
-    "SHOW server_version;"
-  );
+  const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
   const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections;"
+    "SHOW max_connections;",
   );
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseName = "local_db";
   const databaseOpenedConnectionsResult = await database.query({
@@ -21,7 +20,7 @@ async function status(request, response) {
   });
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
-  
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
